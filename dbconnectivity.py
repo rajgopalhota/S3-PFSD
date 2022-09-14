fbbe33e6974866323fc89021bf55f405964e91d2
rajgopalhota <rajgopalhota@gmail.com>
2022-09-14 16:46:20 +0530

    Created repo from pyCharm

--- dbconnectivity.py
+++ dbconnectivity.py
@@ -18,7 +18,42 @@ student2 = [{"regd":"222","Name":"Raj"},
 
 #Insert Documents
 #Insert One Doc
-studentdata.insert_one(student1)
+# studentdata.insert_one(student1)
+#
+# #Insert Many Docs
+# studentdata.insert_many(student2)
 
-#Insert Many Docs
-studentdata.insert_many(student2)
+
+
+#Retrieve document
+#Single Doc
+print(studentdata.find_one())
+#Multi Doc
+for i in studentdata.find():
+    print(i)
+#Search based on condition
+print("\n\nBased on Name search....")
+a = {"Name":"Raja"}
+for i in studentdata.find(a):
+    print(i)
+
+print("\n\nDeleting regd 222................")
+#Delete documents
+b = {"regd":"222"}
+studentdata.delete_one(b)
+for i in studentdata.find():
+    print(i)
+
+
+print("\n\nDeleting multiple data................")
+c = {"Name":"Raj"}
+studentdata.delete_many(c)
+for i in studentdata.find():
+    print(i)
+
+print("\n\nUpdating 444 with a diff name.................")
+#Updating 444 with a diff name
+up = {"Name":"Pooj"}
+studentdata.update_one(up,{"$set":{"Name":"Kanha"}})
+for i in studentdata.find():
+    print(i)

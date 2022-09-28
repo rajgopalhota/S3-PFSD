9f753f02606c43249dc729f553d62d3715b21d67
rajgopalhota <rajgopalhota@gmail.com>
2022-09-28 14:07:16 +0530

    Created repo from pyCharm

--- dynamicRoutingFlask.py
+++ dynamicRoutingFlask.py
@@ -11,41 +11,60 @@ app = Flask(__name__)
 def sample():
     return "<h1>WELCOME TO FLASK</h1>"
 
-# #dynamic routing
-# @app.route("/<name>")
-# def sample2(name):
-#     return f'Hello {name}'
-#
-#
-# #template rendering
-# @app.route("/template")
-# def sample3():
-#     return render_template('index.html')
-# #Give the template directeory in address bar to get index.html
-#
-#
-# #for redirect /route has to be placed
-# @app.route("/template1/<name>")
-# def sample5(name):
-#     return render_template('index1.html',name = name)
-# @app.route("/route/template2/<role>")
-# def sample4(role):
-#     if role == "guest":
-#         return redirect(url_for('sample5',name = role))
-#     else:
-#         return redirect(url_for('sample3'))
+#dynamic routing
+@app.route("/<name>")
+def sample2(name):
+    return f'Hello {name}'
+
+
+#template rendering
+@app.route("/template")
+def sample3():
+    return render_template('index.html')
+#Give the template directeory in address bar to get index.html
+
+
+#for redirect /route has to be placed
+@app.route("/template1/<name>")
+def sample5(name):
+    return render_template('index1.html',name = name)
+@app.route("/route/template2/<role>")
+def sample4(role):
+    if role == "guest":
+        return redirect(url_for('sample5',name = role))
+    else:
+        return redirect(url_for('sample3'))
 #
 # #List rendering with for tag
-# @app.route("/list/rendering")
-# def sample6():
-#     lst = ['abc', 'def', 'ghi']
-#     return render_template('index2.html', name = lst)
-#
+@app.route("/list/rendering")
+def sample6():
+    lst = ['abc', 'def', 'ghi']
+    return render_template('index2.html', name = lst)
+
 
 #Template inheritance
 @app.route("/greeting/abc")
 def sample7():
     return render_template('home.html')
+
+
+
+#Conditional rendering with if tag
+@app.route("/conditional/def/")
+def sample8():
+    role = 'something'
+    return render_template('iftag.html',role = role)
+
+
+
+
+#Flask - Form example
+@app.route('/form/studentform')
+def sample9():
+    return render_template('studentform.html')
+
+
+#Flask Form handling using flask
 
 if __name__ == "__main__":
     app.run()
